35cfe9a7875de23eeb5c95f461d4414fe742b1d6
cspenc <crs992@gmail.com>
2017-06-27 23:26:59 -0400

    further in animation

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-var fullCup = ('#fullCup');
+var fullCup = ('#fullCup'),
+  text1 = ('#text1');
+  text2 = ('#text2');
+  text3 = ('#text3');
+  cta = ('#cta');
 
 var tl = new TimelineLite();
 
 tl.set(fullCup, {scale: .95, x: -65, y: -50})
-  .to(fullCup, 3, {scale: 0.8, x: -175, y: -345, delay: 2})
-  .to(fullCup, 3, {scale: 0.5, x: -210, y: -385, delay: 2})
+  .set(text1, {autoAlpha: 0})
+  .to(fullCup, 2, {scale: 0.8, x: -175, y: -345, delay: 2})
+  .to(fullCup, 2, {scale: 0.5, x: -210, y: -385, delay: 2})
+  .to(fullCup, 2, {scale: 0.5, x: -45, y: -395})
+  .to(text1, 1, {autoAlpha: 1, delay: 3})
+  .to(text1, 1, {autoAlpha: 0})
+  .to(fullCup, 2, {scale: 0.15, x: -150, y: -275})
