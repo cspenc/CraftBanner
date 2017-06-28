024ee0167dc841f775329b45c9aa5549dd9a5cea
cspenc <crs992@gmail.com>
2017-06-28 16:42:02 -0400

    finished

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ var tl = new TimelineLite();
 
 tl.set(fullCup, {scale: .95, x: -65, y: -50})
   .set(halfCup, {autoAlpha:0, scale: 0.20, x: -150, y: -285})
-  .to(fullCup, 2, {scale: 0.8, x: -175, y: -345, delay: 2})
-  .to(fullCup, 2, {scale: 0.5, x: -210, y: -385, delay: 2})
-  .to(fullCup, 2, {scale: 0.5, x: -45, y: -395})
+  .to(fullCup, 1, {scale: 0.8, x: -175, y: -345, delay: 1})
+  .to(fullCup, 1, {scale: 0.5, x: -210, y: -385, delay: 1})
+  .to(fullCup, 1, {scale: 0.5, x: -45, y: -395})
   .to(text1, 1, {autoAlpha: 1})
   .to(text1, 1, {autoAlpha: 0, delay: 3})
   .to(fullCup, 2, {scale: 0.20, x: -150, y: -285})
